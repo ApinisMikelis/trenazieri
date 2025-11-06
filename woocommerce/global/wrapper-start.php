bef4fcd74a2c1ecb8115924d46f7b17d267a8fcd
ApinisMikelis <m.apinis92@gmail.com>
2025-11-06 13:11:04 +0200

    Added category page description

--- woocommerce/global/wrapper-start.php
+++ woocommerce/global/wrapper-start.php
@@ -16,7 +16,7 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 
 
@@ -26,37 +26,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<div class="tre-container">
 		<div class="inner">
 			<h1><?php woocommerce_page_title(); ?></h1>
+      <?php
+        if ( is_product_category() ) {
+          $description = term_description();
+      
+          if ( $description ) {
+              echo '<div class="product-category-description">';
+              echo $description;
+              echo '</div>';
+          }
+        }
+      ?>
 		</div>
 	</div>
 </div>
 <?php endif;?>
 <?php
-
-//$template = wc_get_theme_slug_for_templates();
-
-/*switch ( $template ) {
-	case 'twentyten':
-		echo '<div id="container"><div id="content" role="main">';
-		break;
-	case 'twentyeleven':
-		echo '<div id="primary"><div id="content" role="main" class="twentyeleven">';
-		break;
-	case 'twentytwelve':
-		echo '<div id="primary" class="site-content"><div id="content" role="main" class="twentytwelve">';
-		break;
-	case 'twentythirteen':
-		echo '<div id="primary" class="site-content"><div id="content" role="main" class="entry-content twentythirteen">';
-		break;
-	case 'twentyfourteen':
-		echo '<div id="primary" class="content-area"><div id="content" role="main" class="site-content twentyfourteen"><div class="tfwc">';
-		break;
-	case 'twentyfifteen':
-		echo '<div id="primary" role="main" class="content-area twentyfifteen"><div id="main" class="site-main t15wc">';
-		break;
-	case 'twentysixteen':
-		echo '<div id="primary" class="content-area twentysixteen"><main id="main" class="site-main" role="main">';
-		break;
-	default:
-		echo '<div id="primary" class="content-area"><main id="main" class="site-main" role="main">';
-		break;
-}*/
