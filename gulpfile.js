29c3dcea6fa8ac98f55e0bf32865af5417502208
ApinisMikelis <m.apinis92@gmail.com>
2026-01-26 18:59:00 +0200

    Recovered missing styles lost by retarded born.lv developers

--- gulpfile.js
+++ gulpfile.js
@@ -8,44 +8,38 @@ const rename = require("gulp-rename");
 const cssmin = require("gulp-css");
 const uglify = require("gulp-uglify");
 
-function compileSass(done) {
-  src("assets/src/scss/main.scss")
+function compileSass() {
+  return src("assets/src/scss/main.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(dest("assets/src/css"));
+}
+
+function compileCSS(done) {
+  // Use a return or done() to handle the async task correctly
+  src(["assets/src/css/main.css"])
+    .pipe(concat("app.css"))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(cssmin())
+    .pipe(dest("assets/dist/css"));
   done();
 }
 
-exports.compileSass = compileSass;
-
-function compileCSS(done) {
-  setTimeout(function () {
-    src(["assets/src/css/main.css"])
-      .pipe(concat("app.css"))
-      .pipe(rename({ suffix: ".min" }))
-      .pipe(cssmin())
-      .pipe(dest("assets/dist/css"));
-    done();
-  }, 300);
-}
-
-exports.compileCSS = compileCSS;
-
-function compileJS(done) {
-  src(["assets/src/js/main.js"])
+function compileJS() {
+  return src(["assets/src/js/main.js"])
     .pipe(concat("app.js"))
     .pipe(rename({ suffix: ".min" }))
     .pipe(uglify())
     .pipe(dest("assets/dist/js/"));
-  done();
 }
-
-exports.compileJS = compileJS;
 
 function watchAll() {
   watch("assets/src/scss/**/*.scss", gulp.series(compileSass, compileCSS));
   watch("assets/src/js/*.js", compileJS);
 }
 
+exports.compileSass = compileSass;
+exports.compileCSS = compileCSS;
+exports.compileJS = compileJS;
 exports.watchAll = watchAll;
 
 gulp.task("default", gulp.series(compileSass, compileCSS, compileJS));
