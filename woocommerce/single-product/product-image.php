e79a630276d1e7cbb81f248267c0fc7ca9bb5a76
ApinisMikelis <m.apinis92@gmail.com>
2026-04-15 10:19:57 +0300

    Product image template updated

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -12,13 +12,15 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 9.7.0
+ * @version 10.5.0
  */
+
+use Automattic\WooCommerce\Enums\ProductType;
 
 defined( 'ABSPATH' ) || exit;
 
 if ( ! function_exists( 'wc_get_gallery_image_html' ) ) {
-	return;
+  return;
 }
 
 global $product;
@@ -26,8 +28,8 @@ global $product;
 $columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
 $post_thumbnail_id = $product->get_image_id();
 $attachment_ids    = $product->get_gallery_image_ids();
-	
-if ( $product->is_type( 'variable' ) ) {
+
+if ( $product->is_type( ProductType::VARIABLE ) ) {
   $variation_ids = $product->get_children();
   
   foreach ( $variation_ids as $variation_id ) {
@@ -44,43 +46,45 @@ if ( $post_thumbnail_id && ! in_array( $post_thumbnail_id, $attachment_ids ) ) {
   array_unshift( $attachment_ids, $post_thumbnail_id );
 }
 
-$wrapper_classes   = apply_filters(
-	'woocommerce_single_product_image_gallery_classes',
-	array(
-		'woocommerce-product-gallery',
-		'woocommerce-product-gallery--' . ( $post_thumbnail_id ? 'with-images' : 'without-images' ),
-		'woocommerce-product-gallery--columns-' . absint( $columns ),
-		'images',
-	)
+$wrapper_classes = apply_filters(
+  'woocommerce_single_product_image_gallery_classes',
+  array(
+    'woocommerce-product-gallery',
+    'woocommerce-product-gallery--' . ( $post_thumbnail_id ? 'with-images' : 'without-images' ),
+    'woocommerce-product-gallery--columns-' . absint( $columns ),
+    'images',
+    'tre-product-single-gallery'
+  )
 );
+
 $youtube_video_url = get_field('youtube_video_url', $product->get_id());
 ?>
 
-<div class="tre-product-single-gallery">
+<div class="<?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>">
   <div class="slider-photos-wrapper">
-	    
+      
     <?php if ( $product->is_on_sale() ) : ?>
       <div class="tags">
         <div class="tag">
           <?php
-            if ( $product->is_type( 'variable' ) ) {
+            if ( $product->is_type( ProductType::VARIABLE ) ) {
               $regular_price_min = $product->get_variation_regular_price( 'min' );
               $regular_price_max = $product->get_variation_regular_price( 'max' );
               $sale_price_min    = $product->get_variation_sale_price( 'min' );
               $sale_price_max    = $product->get_variation_sale_price( 'max' );
               
-              $discount_min = round( ( ( $regular_price_min - $sale_price_min ) / $regular_price_min ) * 100 );
-              $discount_max = round( ( ( $regular_price_max - $sale_price_max ) / $regular_price_max ) * 100 );
+              $discount_min = $regular_price_min > 0 ? round( ( ( $regular_price_min - $sale_price_min ) / $regular_price_min ) * 100 ) : 0;
+              $discount_max = $regular_price_max > 0 ? round( ( ( $regular_price_max - $sale_price_max ) / $regular_price_max ) * 100 ) : 0;
               
               if ( $discount_min === $discount_max || $discount_max <= $discount_min ) {
                 echo '-' . $discount_min . '%';
               } else {
                 echo '-' . $discount_min . '% - ' . $discount_max . '%';
               }
-            } else if ($product->is_type( 'simple' )) {
+            } else if ($product->is_type( ProductType::SIMPLE )) {
               $regular_price       = $product->get_regular_price();
               $sale_price          = $product->get_sale_price();
-              $discount_percentage = round( ( ( $regular_price - $sale_price ) / $regular_price ) * 100 );
+              $discount_percentage = $regular_price > 0 ? round( ( ( $regular_price - $sale_price ) / $regular_price ) * 100 ) : 0;
               echo '-' . $discount_percentage . '%';
             }
           ?>
@@ -89,11 +93,17 @@ $youtube_video_url = get_field('youtube_video_url', $product->get_id());
     <?php endif; ?>
 
     <div class="slider-photos" style="opacity: 0;">
-      <?php foreach ($attachment_ids as $attachment_id) : ?>
-        <div class="slider-item" data-image-id="<?php echo $attachment_id; ?>">
-          <?php echo born_acf_image($attachment_id,'tren_woocommerce_single',true); ?>
+      <?php if ( ! empty( $attachment_ids ) ) : ?>
+        <?php foreach ($attachment_ids as $attachment_id) : ?>
+          <div class="slider-item" data-image-id="<?php echo $attachment_id; ?>">
+            <?php echo born_acf_image($attachment_id, 'tren_woocommerce_single', true); ?>
+          </div>
+        <?php endforeach; ?>
+      <?php else : ?>
+        <div class="slider-item">
+          <img src="<?php echo esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ); ?>" alt="<?php esc_attr_e( 'Awaiting product image', 'woocommerce' ); ?>" />
         </div>
-      <?php endforeach; ?>
+      <?php endif; ?>
     </div>
 
     <div class="slider-thumbs-wrapper">
@@ -104,39 +114,37 @@ $youtube_video_url = get_field('youtube_video_url', $product->get_id());
           $image_url_x2 = get_custom_image_url($attachment_id, 'tre-woo-gal-thumb-x2', 208, 272);
 
           if (empty($image_url)) {
-            $image_url = born_acf_image($attachment_id,'tre-woo-gal-thumb',false);
+            $image_url = born_acf_image($attachment_id, 'tre-woo-gal-thumb', false);
           }
           
           if (empty($image_url_x2)) {
-            $image_url_x2 = born_acf_image($attachment_id,'tre-woo-gal-thumb-x2',false);
+            $image_url_x2 = born_acf_image($attachment_id, 'tre-woo-gal-thumb-x2', false);
           }
           ?>
           <button type="button" class="slider-item" data-image-id="<?php echo $attachment_id; ?>">
-            <img srcset="<?php echo $image_url;?>, <?php echo $image_url_x2;?>?v=2 2x" alt="<?php the_title();?>">
+            <img srcset="<?php echo esc_url($image_url); ?>, <?php echo esc_url($image_url_x2); ?>?v=2 2x" alt="<?php echo esc_attr(get_the_title()); ?>">
           </button>
         <?php endforeach; ?>
 
-          <?php if ($youtube_video_url):?>
-            <div class="video-item" data-lightbox="tre-lightbox-gallery">
-              <?php
-                $youtube_id = born_extract_youtube_id( $youtube_video_url);
-                $fallback = get_template_directory_uri() . '/assets/tmp/product-video-thumb-1.jpg';
-                $thumb = $youtube_id 
-                  ? "https://i.ytimg.com/vi/{$youtube_id}/maxresdefault.jpg" 
-                  : get_template_directory_uri() . '/assets/tmp/product-video-thumb-1.jpg';
-
-              ?>
-                <img
-                  src="<?php echo $thumb; ?>" 
-                  onerror="this.src='<?php echo $fallback; ?>'"
-                  alt="<?php the_title();?>"
-                  style="width:100%; height:100%; object-fit:cover; object-position:center;display:block;"
-                />
-            </div>
-          <?php endif;?>
-        </div>
+        <?php if ($youtube_video_url):?>
+          <div class="video-item" data-lightbox="tre-lightbox-gallery">
+            <?php
+              $youtube_id = born_extract_youtube_id( $youtube_video_url);
+              $fallback = get_template_directory_uri() . '/assets/tmp/product-video-thumb-1.jpg';
+              $thumb = $youtube_id 
+                ? "https://i.ytimg.com/vi/{$youtube_id}/maxresdefault.jpg" 
+                : $fallback;
+            ?>
+            <img
+              src="<?php echo esc_url($thumb); ?>" 
+              onerror="this.src='<?php echo esc_url($fallback); ?>'"
+              alt="<?php echo esc_attr(get_the_title()); ?>"
+              style="width:100%; height:100%; object-fit:cover; object-position:center;display:block;"
+            />
+          </div>
+        <?php endif;?>
+      </div>
     </div>
-
   </div>
 </div>
 
@@ -146,7 +154,7 @@ $youtube_video_url = get_field('youtube_video_url', $product->get_id());
       <div class="tre-lightbox-popup">
         <iframe
           id="video123"
-          src="https://www.youtube.com/embed/<?php echo $youtube_id;?>"
+          src="https://www.youtube.com/embed/<?php echo esc_attr($youtube_id); ?>"
           title="YouTube video"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -157,7 +165,7 @@ $youtube_video_url = get_field('youtube_video_url', $product->get_id());
     </div>
   </div>
 <?php endif;?>
- 
+
 <script>
   jQuery(document).ready(function ($) {
     if (jQuery('.tre-product-single-gallery').length) {
